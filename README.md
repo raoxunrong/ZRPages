e109c60c21dd01a7f9719fde5a100b2c82249634
minchina <minchina91@gmail.com>
2015-03-27 12:29:27 +0800

    [Ningchuan] delete dirty code

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ZRPages
-Non-functional testing framework for page objects via webdriver.
+##Overview
+ Non-functional testing framework for page objects via webdriver.
+
+
+##Task1
+
+1. Mark wrong word highlight
+2. Look for English lexicon
--- src/main/java/dear/zr/page/utils/MarkUtil.java
+++
@@ -1,9 +0,0 @@
-package dear.zr.page.utils;
-
-public class MarkUtil {
-    public static String mark(String text, String flag) {
-        int start = text.indexOf(flag);
-        int end = start + flag.length();
-        return "\"" + text.substring(0, start) + "<span style='background-color:red'>" + text.substring(start, end) + "</span>" + text.substring(end) + "\"";
-    }
-}
