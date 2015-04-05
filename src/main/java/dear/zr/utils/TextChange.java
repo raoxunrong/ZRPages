e6d342b0f13c4212bad0e40a76e7d103b3399e76
habens <right.jie@gmail.com>
2015-04-05 11:31:27 +0800

    [Wenjie] remove useless files (TextChange.java & TextChangeTest.java)

--- src/main/java/dear/zr/utils/TextChange.java
+++
@@ -1,8 +0,0 @@
-package dear.zr.utils;
-
-public class TextChange {
-
-    public static String insertAGroupOfTagToText(String text, int start, int end, String startTag, String endTag) {
-        return text.substring(0, start) + startTag + text.substring(start, end) + endTag + text.substring(end);
-    }
-}
