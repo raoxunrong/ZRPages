bcbf87c5e02f40c5fa8a86900f323b0ebb75a2ba
minchina <minchina91@gmail.com>
2015-04-06 17:28:05 +0800

    [Ningchuan] remove internal zr tag

--- src/main/java/dear/zr/utils/Revert.java
+++ src/main/java/dear/zr/utils/Revert.java
@@ -4,15 +4,19 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 
 public class Revert {
+    public static String zr_node = "zr_node";
 
+    private static String generateRevertJs() {
+        String allZrNode = "var allZrNode = document.getElementsByClassName('" + zr_node + "');";
+        String removeStyle = "while(allZrNode.length!=0){" +
+                "var text = allZrNode[allZrNode.length-1].innerHTML;    " +
+                "var newTextNode = document.createTextNode(text);"+
+                "allZrNode[allZrNode.length-1].parentNode.replaceChild(newTextNode,allZrNode[allZrNode.length-1])}";
 
-    public static String generateRevertJs(){
-        String allZrNode = "var allZrNode = document.getElementsByClassName('zr_node');";
-        String removeStyle = "for(var i=0;i<allZrNode.length;i++){allZrNode[i].removeAttribute('style')}";
         return allZrNode + removeStyle;
     }
 
-    public static void revert(WebDriver webDriver){
+    public static void revert(WebDriver webDriver) {
         JavascriptExecutor executor = (JavascriptExecutor) webDriver;
         executor.executeScript(generateRevertJs());
     }
